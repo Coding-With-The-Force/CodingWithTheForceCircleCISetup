18bf1f513cf2d50531f4f3c106d6e425b7cd10dd
Matt Gerry <codingwiththeforce@gmail.com>
2020-09-23 01:07:57 -0500

    updated generate keys script to work with windows

--- build/generate-keys.sh
+++ build/generate-keys.sh
@@ -1,7 +1,7 @@
 echo "Generating certificates for use with CircleCI, press enter to continue"
 read check1
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
+openssl genrsa -des3 -passout pass:MattyG1989 -out server.pass.key 2048
+openssl rsa -passin pass:MattyG1989 -in server.pass.key -out server.key
 rm server.pass.key
 echo "We will now generate the server key, when promoted for a password, press enter"
 echo "press enter to continue"
@@ -11,8 +11,12 @@ echo "we will now generate the certificates, press enter to continue"
 openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
 echo "the key will now be encoded in BASE64 and displayed, use the output for the value of SFDC_SERVER_KEY enviornment variable"
 echo "/n"
-base64 server.key
+echo "press enter to continue"
+read check4
+certutil -encode server.key encodedkey.txt
 echo "we will now clean up, keys will be deleted"
+echo "press enter to continue"
+read check4
 rm server.csr
 rm server.key
 echo "certificate and key generateion complete, please add server.crt to your OAuth connected app in salesforce"

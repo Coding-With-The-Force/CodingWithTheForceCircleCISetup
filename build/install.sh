5e74719948791fdfb22abe2ad4ab340ff84bf567
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-20 23:54:54 -0500

    install script to answer y to installing scanner

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,6 @@
 #Install dependencies, SFDX CLI in this case
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
+
+#Piping in the value y and a new line when prompted for a digital signature installing this plugin
 echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
--- build/runsfdxscanner.sh
+++ build/runsfdxscanner.sh
@@ -1,3 +1,4 @@
 #This script is used to run the sfdx scanner plugin.
-
+#Piping in the value y and a new line when prompted for a digital signature installing this plugin
+echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
 sfdx scanner:run --target "**/default/**" --format "csv" --outfile "/scanner_results/mostrecentdeploy.csv" 

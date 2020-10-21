6a9364384c2d021aa749c55fa843d7930baccbd4
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 10:54:02 -0500

    setting this up with different org

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -1,7 +1,7 @@
 # Get the private key from the environment variable
 echo "Setting up UAT Connection..."
 mkdir keys
-echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
+echo $CIRCLE_TEST_SERVER_KEY | base64 -di > keys/server.key
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a UAT
+sfdx force:auth:jwt:grant --clientid $CIRCLE_TEST_CLIENT_ID --jwtkeyfile keys/server.key --username $CIRCLE_TEST_USERNAME --setdefaultdevhubusername -a UAT

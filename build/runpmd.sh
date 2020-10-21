2a119633c8cb98d530565a82dc1469ee5717fdde
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 09:28:10 -0500

    incorporating pmd

--- build/runpmd.sh
+++ build/runpmd.sh
@@ -1,14 +1,8 @@
 #This script is used to run the sfdx scanner plugin.
-<<<<<<< HEAD
+
 echo "Running PMD"
 npx pmd -d ./force-app/main/default -f html -R /apexrules.xml -r ./scanner_results/results.html
-=======
-echo "PMD Help"
-npx pmd -help
 
-echo "PMD Running"
-npx pmd -d ./force-app/main/default -f html -R ./build/apexrules.xml -r ./scanner_results/results.html
->>>>>>> d9f3b7b46d7a8b37ca66740f50326fc8a052d193
 #echo "Installing JDK"
 #sudo apt-get install openjdk-8-jdk
 

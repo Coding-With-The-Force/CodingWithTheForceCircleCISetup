f1bfff1d184bb0696bde2bbf876d031e5f8327fa
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 01:03:36 -0500

    incorporating pmd

--- build/runsfdxscanner.sh
+++ build/runsfdxscanner.sh
@@ -7,7 +7,7 @@ sudo apt-get install openjdk-8-jdk
 
 echo "Installing sfdx scanner"
 #Piping in the value y and a new line when prompted for a digital signature installing this plugin
-echo -e 'y\n' | sudo sfdx plugins:install -global @salesforce/sfdx-scanner
+echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running sfdx scanner"
 sfdx scanner:run --target "**/default/**" --format "csv" --outfile "./scanner_results/mostrecentdeploy.csv" 

6c31783f456bc5551ef945bf91106fbcbd355367
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 00:48:46 -0500

    incorporating pmd

--- build/install.sh
+++ build/install.sh
@@ -5,31 +5,6 @@ sudo apt-get install openjdk-8-jdk
 echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
-echo "Initializing node"
-spawn npm init
-expect "package name:"
-send "\n" 
-expect "version:"
-send "\n" 
-expect "description:"
-send "\n" 
-expect "entry point:"
-send "\n" 
-expect "test command:"
-send "\n" 
-expect "git repository"
-send "\n" 
-expect "keywords:"
-send "\n" 
-expect "author:"
-send "\n" 
-expect "license:"
-send "\n" 
-expect "Is this ok?"
-send "\n"
-expect eof
-!
-
 echo "Installing PMD"
 sudo npm install pmd-bin
 

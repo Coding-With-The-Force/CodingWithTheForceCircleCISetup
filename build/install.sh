125e9e9eb9193e6b5ecd1932962a4f02a88f90b1
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 00:29:29 -0500

    incorporating pmd

--- build/install.sh
+++ build/install.sh
@@ -1,10 +1,13 @@
 #Install dependencies, SFDX CLI in this case
+echo "Installing JDK"
 sudo apt-get install openjdk-8-jdk
 
-echo "Installing Dependencies... "
+echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
+echo "Installing PMD"
 sudo npm install pmd-bin
 
+echo "Installs finished"
 #Piping in the value y and a new line when prompted for a digital signature installing this plugin
 #echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner

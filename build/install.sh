540b4ec5452f5fa30aac701a8dcc2edaea6468dd
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 00:58:17 -0500

    incorporating pmd

--- build/install.sh
+++ build/install.sh
@@ -1,12 +1,12 @@
-#Install dependencies, SFDX CLI in this case
+#Installing all dependencies, must be installed globally
 echo "Installing JDK"
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install -global openjdk-8-jdk
 
 echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
-echo "Installing PMD"
-sudo npm install -global pmd-bin
+#echo "Installing PMD"
+#sudo npm install -global pmd-bin
 
 echo "Installing sfdx scanner"
 #Piping in the value y and a new line when prompted for a digital signature installing this plugin
--- build/runsfdxscanner.sh
+++ build/runsfdxscanner.sh
@@ -1,7 +1,7 @@
 #This script is used to run the sfdx scanner plugin.
 
-echo "Running PMD"
-pmd -d ./force-app/main/default -f html -R ./build/apexrules.xml -r ./scanner_results/results.html
+#echo "Running PMD"
+#pmd -d ./force-app/main/default -f html -R ./build/apexrules.xml -r ./scanner_results/results.html
 
 echo "Running sfdx scanner"
 sfdx scanner:run --target "**/default/**" --format "csv" --outfile "/scanner_results/mostrecentdeploy.csv" 

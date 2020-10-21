c70eb42e30ab647fdba67578c55c6bf71b20b5b8
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 00:39:02 -0500

    incorporating pmd

--- build/install.sh
+++ build/install.sh
@@ -6,7 +6,7 @@ echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
 echo "Initializing node"
-echo -e '\n\n\n\n\n\n\n\n' | sudo npm init
+echo -e '\n' | sudo npm init
 
 echo "Installing PMD"
 sudo npm install pmd-bin

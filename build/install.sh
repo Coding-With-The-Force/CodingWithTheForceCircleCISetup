20d261ee42fd85de35d9e0c1c4f09be59da82428
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-20 23:26:38 -0500

    install script to answer y to installing scanner

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,4 @@
 #Install dependencies, SFDX CLI in this case
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
-sudo sfdx plugins:install @salesforce/sfdx-scanner -y
+sudo yes | sfdx plugins:install @salesforce/sfdx-scanner

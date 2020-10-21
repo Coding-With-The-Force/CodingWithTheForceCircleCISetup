f5dfeca915748c550b8769c1a52d2d004783fb72
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-20 23:16:40 -0500

    install script to answer y to installing scanner

--- build/install.sh
+++ build/install.sh
@@ -2,4 +2,4 @@
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
 sudo sfdx plugins:install @salesforce/sfdx-scanner
-expect "This plugin is not digitally signed and its authenticity cannot be verified. Continue installation y/n?" { send "\r" }
+echo "y \r" 

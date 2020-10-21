87cc47b03a73819b65ea7504c1bd5ab74b880c41
Matt Gerry <codingwiththeforce@gmail.com>
2020-10-21 01:11:01 -0500

    incorporating pmd

--- build/runsfdxscanner.sh
+++ build/runsfdxscanner.sh
@@ -1,13 +1,16 @@
 #This script is used to run the sfdx scanner plugin.
 
-#echo "Running PMD"
-#pmd -d ./force-app/main/default -f html -R ./build/apexrules.xml -r ./scanner_results/results.html
+echo "Installing PMD"
+npm install pmd-bin
+
+echo "Running PMD"
+pmd -d ./force-app/main/default -f html -R ./build/apexrules.xml -r ./scanner_results/results.html
 #echo "Installing JDK"
 #sudo apt-get install openjdk-8-jdk
 
-echo "Installing sfdx scanner"
+#echo "Installing sfdx scanner"
 #Piping in the value y and a new line when prompted for a digital signature installing this plugin
-echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
+#echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
 
-echo "Running sfdx scanner"
-sfdx scanner:run --target "**/default/**" --format "csv" --outfile "./scanner_results/mostrecentdeploy.csv" 
+#echo "Running sfdx scanner"
+#sfdx scanner:run --target "**/default/**" --format "csv" --outfile "./scanner_results/mostrecentdeploy.csv" 

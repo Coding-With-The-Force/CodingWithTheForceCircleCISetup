2d4da0750207febd317576aa690b8ecf4e727c6e
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-28 20:19:25 -0600

    runpmd changes

--- build/installpmd.sh
+++ build/installpmd.sh
@@ -1,3 +1,7 @@
-echo "Installing PMD"
-npm install node-jre
-npm install pmd-bin
+#echo "Installing PMD"
+#npm install node-jre
+#npm install pmd-bin
+
+echo "Installing sfdx scanner"
+#Piping in the value y and a new line when prompted for a digital signature installing this plugin
+echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
--- build/runpmd.sh
+++ build/runpmd.sh
@@ -1,15 +1,11 @@
 #This script is used to run the sfdx scanner plugin.
 
-echo "Running PMD"
-echo $PWD
-npx pmd -d . -f csv -R $RULESET -r analysis.csv
+#echo "Running PMD"
+#echo $PWD
+#npx pmd -d . -f csv -R $RULESET -r analysis.csv
 
 #echo "Installing JDK"
 #sudo apt-get install openjdk-8-jdk
 
-#echo "Installing sfdx scanner"
-#Piping in the value y and a new line when prompted for a digital signature installing this plugin
-#echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
-
-#echo "Running sfdx scanner"
-#sfdx scanner:run --target "**/default/**" --format "csv" --outfile "./scanner_results/mostrecentdeploy.csv" 
+echo "Running sfdx scanner"
+npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "analysis.csv" 

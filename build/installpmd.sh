7aff4b8fb38922117f964e332ad2a40f7dbb023f
Matt Gerry <codingwiththeforce@gmail.com>
2020-12-28 20:24:13 -0600

    runpmd changes

--- build/installpmd.sh
+++ build/installpmd.sh
@@ -4,4 +4,4 @@
 
 echo "Installing sfdx scanner"
 #Piping in the value y and a new line when prompted for a digital signature installing this plugin
-echo -e 'y\n' | sudo sfdx plugins:install @salesforce/sfdx-scanner
+echo -e 'y\n' | npx sfdx plugins:install @salesforce/sfdx-scanner
